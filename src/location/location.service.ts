faeb6cb3e10b463f6a6a5b828d6f78c6683d2ad0
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-05-26 22:06:33 +0900

    Refactor: Refactor location.service.ts and stores.controller.ts

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -3,34 +3,8 @@ import axios from 'axios';
 
 @Injectable()
 export class LocationService {
-  // async getCurrentLocation() {
-  //   try {
-  //     const response = await axios.get("http://ip-api.com/json");
-  //     const { city, regionName, country } = response.data;
-  //     console.log()
-  //   }
-  // }
-  async getPublicIpAddress(): Promise<{ lat: number; lon: number }> {
-    try {
-      const response = await axios.get('http://ip-api.com/json/');
-      const { lat, lon } = response.data;
-
-      return { lat, lon };
-      // const { query } = response.data;
-      // return query;
-    } catch (error) {
-      console.error('Error retrieving public IP address:', error);
-      throw error;
-    }
-  }
-
-  async getLatAndLonOfIpAddress(
-    ipAddress: string,
-  ): Promise<{ latitude: number; longitude: number }> {
-    const API_KEY = 'F8836A600EB5299CF89F9D127A6BFC2C';
-    const response = await axios.get(
-      `https://api.ip2location.io/?key=${API_KEY}&ip=${ipAddress}`,
-    );
+  async getCoordinatesOfIpAddress(): Promise<any> {
+    const response = await axios.get('https://get.geojs.io/v1/ip/geo.json');
 
     const { latitude, longitude } = response.data;
 
